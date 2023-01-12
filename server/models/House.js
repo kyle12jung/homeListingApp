03b0fe7db755de0d1b4b5ee36a12217224d63049
Kyle Jung <kyle12jung@gmail.com>
2023-01-11 18:59:16 -0600

    changes to the house schema and the houses routes, need to be tested

--- server/models/House.js
+++ server/models/House.js
@@ -3,11 +3,20 @@ const mongoose = require('mongoose')
 const HouseSchema = new mongoose.Schema({
     title: { type: String, required: true },
     address: { type: String, required: true },
-    homeType: String,
+    bedroom: { type: Number, required: true },
+    bathroom: { type: Number, required: true },
     description: String,
     price: { type: Number, required: true },
-    image: String,
-    yearBuilt: Number,
+    image: [{ type: mongoose.Schema.Types.ObjectId, ref: "File" }],
 })
+
+const fileSchema = new mongoose.Schema({
+    filename: { type: String, required: true },
+    contentType: { type: String, required: true },
+    metadata: { type: Object },
+    uploadDate: { type: Date, default: Date.now }
+});
+
+const File = mongoose.model("File", fileSchema);
 
 module.exports = mongoose.model('House', HouseSchema)
--- server/routes/houses.js
+++ server/routes/houses.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const { body, validationResult } = require('express-validator');
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const Grid = require("gridfs");
 
-const House = require('../models/House')
+const House = require("../models/House");
 
-const router = express.Router()
+const router = express.Router();
 
 // /api/houses
-router.post('/',
-    body('title').isLength({ min: '3', max: '50' }).withMessage('Title should be between 3 to 50 characters'),
-    body('description').isLength({ min: '10', max: '200' }).withMessage('Description should be between 10 to 200 characters'),
-    body('address').isLength({ min: '10', max: '100' }).withMessage('Address should be between 10 to 100 characters'),
-    body('price').isNumeric().withMessage('Price should be a number'),
-    (req, res) => {
-        const errors = validationResult(req)
+router.post(
+    "/",
+    body("title")
+    .isLength({ min: "3", max: "20" })
+    .withMessage("Title should be between 3 to 20 characters"),
+    body("description")
+    .isLength({ min: "10", max: "200" })
+    .withMessage("Description should be between 10 to 200 characters"),
+    body("address")
+    .isLength({ min: "10", max: "100" })
+    .withMessage("Address should be between 10 to 100 characters"),
+    body("price").isNumeric().withMessage("Price should be a number"),
+    body("bedroom").isNumeric().withMessage("Price should be a number"),
+    body("bathroom").isNumeric().withMessage("Price should be a number"),
+    async(req, res) => {
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
@@ -20,81 +30,155 @@ router.post('/',
         const house = new House({
             title: req.body.title,
             address: req.body.address,
-            homeType: req.body.homeType,
+            bedroom: req.body.bedrooms,
+            bathroom: req.body.bathrooms,
             description: req.body.description,
             price: req.body.price,
-            image: req.body.image,
-            yearBuilt: req.body.yearBuilt,
-        })
+        });
 
-        house.save()
-            .then(result => {
+        // Upload and store the image(s) in gridfs
+        const files = req.files;
+        if (files) {
+            house.images = await Promise.all(
+                files.map(async(file) => {
+                    const writestream = gfs.createWriteStream({
+                        filename: file.name,
+                        mode: "w",
+                        content_type: file.mimetype,
+                    });
+                    await fs.createReadStream(file.path).pipe(writestream);
+                    const uploadedFile = await new Promise((resolve, reject) => {
+                        writestream.on("close", async(file) => {
+                            try {
+                                const uploadedFile = new File({
+                                    filename: file.filename,
+                                    contentType: file.contentType,
+                                    metadata: req.body.metadata,
+                                });
+                                await uploadedFile.save();
+                                resolve(uploadedFile._id);
+                            } catch (error) {
+                                reject(error);
+                            }
+                        });
+                    });
+                    return uploadedFile;
+                })
+            );
+        }
+
+        await house
+            .save()
+            .then((result) => {
                 res.send({
                     message: "House data created",
                     data: result,
-                })
+                });
             })
-            .catch(err => console.log(err))
-    })
+            .catch((err) => console.log(err));
+    }
+);
 
 // /api/houses
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     House.find()
-        .then(houses => {
-            res.send(houses)
+        .then((houses) => {
+            res.send(houses);
         })
-        .catch(err => console.log(err))
-})
+        .catch((err) => console.log(err));
+});
 
 // api/houses/id
-router.get('/:id', (req, res) => {
-    const houseID = req.params.id
-    House.findById(houseID)
-        .then(house => {
-            res.send(house)
-        })
-        .catch(err => console.log(err))
-})
-
+router.get("/:id", async(req, res) => {
+    // const houseID = req.params.id
+    // House.findById(houseID)
+    //     .then(house => {
+    //         res.send(house)
+    //     })
+    //     .catch(err => console.log(err))
+    const house = await House.findById(req.params.id);
+    if (!house) {
+        return res.status(404).json({
+            error: "No house found"
+        });
+    }
+    await res.send(house);
+});
 
 // api/houses/id
-router.put('/:id',
-    body('title').isLength({ min: '3', max: '50' }).withMessage('Title should be between 3 to 50 characters'),
-    body('description').isLength({ min: '10', max: '200' }).withMessage('Description should be between 10 to 200 characters'),
-    body('address').isLength({ min: '10', max: '100' }).withMessage('Address should be between 10 to 100 characters'),
-    body('price').isNumeric().withMessage('Price should be a number'),
-    (req, res) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+// router.put(
+//     "/:id",
+//     body("title")
+//     .isLength({ min: "3", max: "50" })
+//     .withMessage("Title should be between 3 to 50 characters"),
+//     body("description")
+//     .isLength({ min: "10", max: "200" })
+//     .withMessage("Description should be between 10 to 200 characters"),
+//     body("address")
+//     .isLength({ min: "10", max: "100" })
+//     .withMessage("Address should be between 10 to 100 characters"),
+//     body("price").isNumeric().withMessage("Price should be a number"),
+//     (req, res) => {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() });
+//         }
+//         const houseID = req.params.id;
+//         House.findById(houseID)
+//             .then((house) => {
+//                 (house.title = req.body.title),
+//                 (house.address = req.body.address),
+//                 (house.homeType = req.body.homeType),
+//                 (house.description = req.body.description),
+//                 (house.price = req.body.price),
+//                 (house.image = req.body.image),
+//                 (house.yearBuilt = req.body.yearBuilt);
+
+//                 return house.save();
+//             })
+//             .then((result) => res.send(result))
+//             .catch((err) => console.log(err));
+//     }
+// );
+
+router.patch(
+    "/:id",
+    body("title")
+    .isLength({ min: "3", max: "50" })
+    .withMessage("Title should be between 3 to 50 characters"),
+    body("description")
+    .isLength({ min: "10", max: "200" })
+    .withMessage("Description should be between 10 to 200 characters"),
+    body("address")
+    .isLength({ min: "10", max: "100" })
+    .withMessage("Address should be between 10 to 100 characters"),
+    body("price").isNumeric().withMessage("Price should be a number"),
+    async(req, res) => {
+        // Find the house by id and handle any errors
+        const house = await House.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!house) {
+            return res.status(404).json({
+                error: "No house found"
+            });
+        }
+        try {
+            const result = await house.save()
+            await res.send(result)
+        } catch (err) {
+            console.log(err)
         }
 
-        const houseID = req.params.id
-        House.findById(houseID)
-            .then(house => {
-                house.title = req.body.title,
-                    house.address = req.body.address,
-                    house.homeType = req.body.homeType,
-                    house.description = req.body.description,
-                    house.price = req.body.price,
-                    house.image = req.body.image,
-                    house.yearBuilt = req.body.yearBuilt
+    });
 
-                return house.save()
-            })
-            .then(result => res.send(result))
-            .catch(err => console.log(err))
-    })
 
 // api/houses/id
-router.delete('/:id', (req, res) => {
-    const houseID = req.params.id
+router.delete("/:id", (req, res) => {
+    const houseID = req.params.id;
     House.findByIdAndDelete(houseID)
-        .then(house => {
-            res.send(house)
+        .then((house) => {
+            res.send(house);
         })
-        .catch(err => console.log(err))
-})
-
+        .catch((err) => console.log(err));
+});
 
 module.exports = router;
